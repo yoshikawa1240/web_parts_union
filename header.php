5bac5e9df2f831493975e7d50c74a60e1efbf8eb
yoshikawa1240 <taka.scholes18@gmail.com>
2021-09-11 18:20:32 +0900

    add reset.css
    リセットCSSの追加

--- header.php
+++ header.php
@@ -9,7 +9,7 @@ $home_url = 'http://localhost:8888/web_parts_union';//トップページのURL�
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>見出し</title>
-    <link rel="stylesheet" href="<?php $home_url;?>/webparts/css/reset.css">
+    <link rel="stylesheet" href="<?php $home_url;?>/web_parts_union/css/reset.css">
     <!-- FontAwesome -->
     <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
 </head>
