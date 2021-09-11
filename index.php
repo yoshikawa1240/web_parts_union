8b7c300fd1f5e4c6eb223dff1bf4317305362b88
yoshikawa1240 <taka.scholes18@gmail.com>
2021-09-11 15:52:49 +0900

    delete cont folder
    contファイルの削除

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php require('./header.php');?>
-    <?php require('./cont/001/index.php');?>
-    <?php require('./cont/002/index.php');?>
-    <?php require('./cont/003/index.php');?>
+  
     <?php require('./web_parts_ty/contents/001/index.php');?>
 <?php require('./footer.php');?>
