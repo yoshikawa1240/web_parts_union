db307998bee5cb9ac1a9ad65881165c9cbdbce51
yoshikawa1240 <taka.scholes18@gmail.com>
2021-09-11 15:35:10 +0900

    create file

---
+++ header.php
@@ -0,0 +1,16 @@
+<?php
+$home_url = 'http://localhost:8888/web_parts_union';//トップページのURLを入力してください
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>見出し</title>
+    <link rel="stylesheet" href="<?php $home_url;?>/webparts/css/reset.css">
+    <!-- FontAwesome -->
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+</head>
+<body>
