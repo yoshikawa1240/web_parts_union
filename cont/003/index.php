92658a6a8dcc81d0cd327dde2938aa475330b948
yoshikawa1240 <taka.scholes18@gmail.com>
2021-09-11 15:39:04 +0900

    create branch
    phpのブランチを作成

---
+++ cont/003/index.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="<?php echo $home_url;?>/cont/003/css/style.min.css" );>
+<link rel="stylesheet" href="<?php echo $home_url;?>/cont/003/js/script.js" );>
+
+<!----------------------------------------- 
+web_parts_003
+------------------------------------------>
+
+<div class="web_parts_003">
+    <div class="cont">
+        <img src="https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg" alt="#">
+        <h2 class="ttl">あらゆる波を乗り越えたその先が知りたい</h2>
+        <p>時代の波に飲み込まれている…、そんな毎日。僕はこのままで良いのか？<br>波を超えたその先を知りたい。自分の未来を自分で作りたい。<br>だからプログラミングを学びます。</p>
+        <a class="btn" href="">ABOUT</a>
+    </div>
+</div>
