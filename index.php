ed818c59c3aeb17dc30a137feecac2f460d2bb32
yoshikawa1240 <taka.scholes18@gmail.com>
2021-09-12 10:42:35 +0900

    union web_parts_contents

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php require('./header.php');?>
-  
+
     <?php require('./web_parts_ty/contents/001/index.php');?>
+    <?php require('./web_parts_ty/contents/002/index.php');?>
+    <?php require('./web_parts_ty/contents/003/index.php');?>
+    <?php require('./web_parts_ty/contents/004/index.php');?>
+    <?php require('./web_parts_ty/contents/005/index.php');?>
+    <?php require('./web_parts_ty/contents/006/index.php');?>
+    <?php require('./web_parts_ty/contents/007/index.php');?>
+    <?php require('./web_parts_ty/contents/008/index.php');?>
+    <?php require('./web_parts_ty/contents/009/index.php');?>
+    <?php require('./web_parts_ty/contents/010/index.php');?>
+    <?php require('./web_parts_ty/contents/011/index.php');?>
+
 <?php require('./footer.php');?>
