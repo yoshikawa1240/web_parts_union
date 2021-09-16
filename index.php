6eb59dfc98c67c83c1d240fb8ccf7ee733a73461
yoshikawa1240 <taka.scholes18@gmail.com>
2021-09-17 06:23:49 +0900

    union web_parts
    Webパーツの結合

--- index.php
+++ index.php
@@ -1,15 +1,20 @@
 <?php require('./header.php');?>
 
+<?php require('./web_parts_ty/contents/005/index.php');?>
+<?php require('./web_parts_ty/contents/011/index.php');?>
+    <?php require('./web_parts_ty/first_view/001/index.php');?>
+    <?php require('./web_parts_ty/first_view/002/index.php');?>
+    <?php require('./web_parts_ty/news/001/index.php');?>
     <?php require('./web_parts_ty/contents/001/index.php');?>
     <?php require('./web_parts_ty/contents/002/index.php');?>
     <?php require('./web_parts_ty/contents/003/index.php');?>
     <?php require('./web_parts_ty/contents/004/index.php');?>
-    <?php require('./web_parts_ty/contents/005/index.php');?>
     <?php require('./web_parts_ty/contents/006/index.php');?>
     <?php require('./web_parts_ty/contents/007/index.php');?>
     <?php require('./web_parts_ty/contents/008/index.php');?>
     <?php require('./web_parts_ty/contents/009/index.php');?>
     <?php require('./web_parts_ty/contents/010/index.php');?>
-    <?php require('./web_parts_ty/contents/011/index.php');?>
+    <?php require('./web_parts_ty/contact/001/index.php');?>
+    <?php require('./web_parts_ty/information/001/index.php');?>
 
 <?php require('./footer.php');?>
